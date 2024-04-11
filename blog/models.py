3fd305557af31f1e30b3d51ca625ad8ce9691434
luci2106 <mixturadosvigo@gmail.com>
2024-04-11 16:43:47 +0200

    actualizado

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    
 
     def publish(self):
         self.published_date = timezone.now()
@@ -18,3 +19,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
